d76ca99f072494b64e087c595371a0123e1d48f2
Balaji-Sak <balajisakthivel240@gamil.com>
2026-01-06 00:36:22 +0530

    Creating Hero section..

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Footer from "./Footer"
+import Hero from "./Hero"
 
 
 function App() {
   return(
     <>
       <Header></Header>
+      <Hero></Hero>
       <Footer></Footer>
     </>  
   );
---
+++ src/Hero.jsx
@@ -0,0 +1,21 @@
+import myPhoto from "./assets/myPhoto.jpg"
+
+
+function Hero() {
+    return (
+        <section className="hero">
+            <div className="hero-text">
+                <h1>Balaji S</h1>
+                <h2>java fullstack developer</h2>
+                <p>“ECE graduate passionate about building modern, scalable web applications.”</p>
+
+            </div>
+            <div className="photo-f">
+                <img src={myPhoto} alt="profile pic" />
+
+            </div>
+        </section>
+    );
+}
+
+export default Hero
